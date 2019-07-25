0e24a7aa354bb81fb49eac0c9a84753e11a13876
Alexander Kulik <a.kulik@dodopizza.com>
2019-07-25 15:37:31 +0300

    Added the authorize guard.

--- src/ClientApp/src/app/app-routing.module.ts
+++ src/ClientApp/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { DataComponent } from "./data/data.component";
+import { AuthorizeGuard } from "./api-authorization/authorize.guard";
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'data', component: DataComponent }
+  { path: 'data', component: DataComponent, canActivate: [AuthorizeGuard] }
 ];
 
 
