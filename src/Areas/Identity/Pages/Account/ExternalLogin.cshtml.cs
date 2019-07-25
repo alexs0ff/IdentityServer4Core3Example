60fbd44cb8060025558d96835505719598625f8b
Alexander Kulik <a.kulik@dodopizza.com>
2019-07-25 15:58:18 +0300

    Changed externallogin page.

--- src/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
+using IdentityModel;
 using Microsoft.AspNetCore.Authorization;
 using IdentityServer4WebApp.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Server.IISIntegration;
 using Microsoft.Extensions.Logging;
 
 namespace IdentityServer4WebApp.Areas.Identity.Pages.Account
@@ -52,9 +56,13 @@ namespace IdentityServer4WebApp.Areas.Identity.Pages.Account
             return RedirectToPage("./Login");
         }
 
-        public IActionResult OnPost(string provider, string returnUrl = null)
+        public async Task<IActionResult> OnPost(string provider, string returnUrl = null)
         {
-            // Request a redirect to the external login provider.
+            if (IISDefaults.AuthenticationScheme == provider)
+            {
+                return await ProcessWindowsLoginAsync(returnUrl);
+            }
+
             var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
             var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
             return new ChallengeResult(provider, properties);
@@ -79,6 +87,10 @@ namespace IdentityServer4WebApp.Areas.Identity.Pages.Account
             var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor : true);
             if (result.Succeeded)
             {
+                var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+                await UpdateClaims(info, user, "hasUsersGroup");
+
                 _logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity.Name, info.LoginProvider);
                 return LocalRedirect(returnUrl);
             }
@@ -124,6 +136,7 @@ namespace IdentityServer4WebApp.Areas.Identity.Pages.Account
                     {
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
+                        await UpdateClaims(info, user, "hasUsersGroup");
                         return LocalRedirect(returnUrl);
                     }
                 }
@@ -137,5 +150,59 @@ namespace IdentityServer4WebApp.Areas.Identity.Pages.Account
             ReturnUrl = returnUrl;
             return Page();
         }
+
+        private async Task<IActionResult> ProcessWindowsLoginAsync(string returnUrl)
+        {
+            var result = await HttpContext.AuthenticateAsync(IISDefaults.AuthenticationScheme);
+            if (result?.Principal is WindowsPrincipal wp)
+            {
+                var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
+
+                var props = _signInManager.ConfigureExternalAuthenticationProperties(IISDefaults.AuthenticationScheme, redirectUrl);
+                props.Items["scheme"] = IISDefaults.AuthenticationScheme;
+
+                var id = new ClaimsIdentity(IISDefaults.AuthenticationScheme);
+                id.AddClaim(new Claim(JwtClaimTypes.Subject, wp.Identity.Name));
+                id.AddClaim(new Claim(JwtClaimTypes.Name, wp.Identity.Name));
+                id.AddClaim(new Claim(ClaimTypes.NameIdentifier, wp.Identity.Name));
+
+                var wi = wp.Identity as WindowsIdentity;
+                var groups = wi.Groups.Translate(typeof(NTAccount));
+                var hasUsersGroup = groups.Any(i => i.Value.Contains(@"BUILTIN\Users", StringComparison.OrdinalIgnoreCase));
+
+                id.AddClaim(new Claim("hasUsersGroup", hasUsersGroup.ToString()));
+
+                await HttpContext.SignInAsync(IdentityConstants.ExternalScheme, new ClaimsPrincipal(id), props);
+
+                return Redirect(props.RedirectUri);
+            }
+
+            return Challenge(IISDefaults.AuthenticationScheme);
+        }
+
+        private async Task UpdateClaims(ExternalLoginInfo info, ApplicationUser user, params string[] claimTypes)
+        {
+            if (claimTypes == null)
+            {
+                return;
+            }
+
+            var claimTypesHash = new HashSet<string>(claimTypes);
+
+            var claims = (await _userManager.GetClaimsAsync(user)).Where(c => claimTypesHash.Contains(c.Type)).ToList();
+
+            await _userManager.RemoveClaimsAsync(user, claims);
+
+            foreach (var claimType in claimTypes)
+            {
+                if (info.Principal.HasClaim(c => c.Type == claimType))
+                {
+                    claims = info.Principal.FindAll(claimType).ToList();
+                    await _userManager.AddClaimsAsync(user, claims);
+                }
+            }
+        }
+
+
     }
 }
