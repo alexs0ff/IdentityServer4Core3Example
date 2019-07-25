6eb635cd63faa65ef1451159eb8d6cf494cb74c6
Alexander Kulik <a.kulik@dodopizza.com>
2019-07-25 15:24:17 +0300

    Imported the ApiAuthorizationModule.

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DataComponent } from './data/data.component';
+import { ApiAuthorizationModule } from "./api-authorization/api-authorization.module";
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { DataComponent } from './data/data.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ApiAuthorizationModule,
     AppRoutingModule
   ],
   providers: [],
